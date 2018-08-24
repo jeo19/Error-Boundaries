ebd56ad06299c9eed09ad5fc48702e24f09a6930
jeo19 <terrakkk@outlook.com>
2018-08-24 22:39:09 +0630

    It's the training for setStateNull(sencond)

--- src/setStateNull.js
+++ src/setStateNull.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+const MAX_PIZZAS = 20;
+const eatPizza = (state, props) => {
+  const { pizzas } = state;
+  if (pizzas < MAX_PIZZAS) {
+    return { pizzas: pizzas + 1 };
+  } else {
+    return null;
+  }
+};
 class Controlled extends Component {
   state = {
-    pizzas: 10
+    pizzas: 0
   };
   render() {
     const { pizzas } = this.state;
     return (
-      <button>{`I have aten ${pizzas} ${
+      <button onClick={this._handleClick}>{`I have eaten ${pizzas} ${
         pizzas === 1 ? 'pizza' : 'pizzas'
       }`}</button>
     );
   }
+  _handleClick = () => {
+    this.setState(eatPizza);
+  };
 }
 class App extends Component {
   render() {
